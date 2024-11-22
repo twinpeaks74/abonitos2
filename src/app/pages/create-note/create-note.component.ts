bed47a640f4199dc650d1ae37269b8ee71c06901
oliver zulett <oliverzulett@gmail.com>
2024-11-22 14:36:35 -0400

    setup basico del formulario

--- src/app/pages/create-note/create-note.component.ts
+++ src/app/pages/create-note/create-note.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
 import { ToolBarComponent } from '../../components/tool-bar/tool-bar.component';
+import { Note } from '../../components/card-list/card-list.component';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-create-note',
-  imports: [ToolBarComponent],
+  imports: [ToolBarComponent, FormsModule],
   templateUrl: './create-note.component.html',
   styleUrl: './create-note.component.css'
 })
 export class CreateNoteComponent {
+  noteCreated: Partial<Note> = {
+    title: '',
+    content: ''
+  }
 
+  onSubmit(form: NgForm): void {
+    if (form.valid) {
+      console.log(this.noteCreated);
+    }
+  }
 }
