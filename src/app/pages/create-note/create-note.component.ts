0524d25af4d34fe70601b698e7f9b7d04d0cbce2
oliver zulett <oliverzulett@gmail.com>
2024-11-22 15:24:10 -0400

    navegar a la lista luego de crear nota

--- src/app/pages/create-note/create-note.component.ts
+++ src/app/pages/create-note/create-note.component.ts
@@ -4,27 +4,27 @@ import { FormsModule, NgForm } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { Note } from '../../models/note.model';
 import { NotesService } from '../../services/notes.service';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-create-note',
-  imports: [ToolBarComponent, FormsModule, CommonModule],
+  imports: [ToolBarComponent, FormsModule, CommonModule, RouterModule],
   templateUrl: './create-note.component.html',
-  styleUrl: './create-note.component.css'
+  styleUrl: './create-note.component.css',
 })
 export class CreateNoteComponent {
   noteCreated: Partial<Note> = {
     title: '',
-    content: ''
-  }
+    content: '',
+  };
 
-  constructor(private noteService: NotesService) {
-
-  }
+  constructor(private noteService: NotesService, private router: Router) {}
 
   onSubmit(form: NgForm): void {
     if (form.valid) {
       console.log(this.noteCreated);
       this.noteService.createNote(this.noteCreated);
+      this.router.navigate(['/']);
     }
   }
 }
