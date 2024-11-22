6eb6cb9090ea5d9bfeaf0fd9a966c809cf2f41d6
oliver zulett <oliverzulett@gmail.com>
2024-11-22 13:27:27 -0400

    asignacion de rutas a componentes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { CreateNoteComponent } from './pages/create-note/create-note.component';
+import { EditNoteComponent } from './pages/edit-note/edit-note.component';
+import { NoteListComponent } from './pages/note-list/note-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', component: NoteListComponent},
+  {path: 'create-note', component: CreateNoteComponent},
+  {path: 'edit-note', component: EditNoteComponent},
+  {path: '**', redirectTo: ''},
+];
