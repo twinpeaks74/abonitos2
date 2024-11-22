2b11317a40bf536a082f5175f8eb7f711c0d0efb
oliver zulett <oliverzulett@gmail.com>
2024-11-22 11:07:24 -0400

    estilizacion de componentes con ngStyle

--- src/app/note-card/note-card.component.ts
+++ src/app/note-card/note-card.component.ts
@@ -1,11 +1,16 @@
 import { Component, Input } from '@angular/core';
 import { Note } from '../card-list/card-list.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
+  standalone: true,
+  imports: [CommonModule],
   selector: 'app-note-card',
   templateUrl: './note-card.component.html',
   styleUrl: './note-card.component.css'
 })
 export class NoteCardComponent {
   @Input() note!: Note;
+  cardHeight = '290px';
+  cardWidth = '380px';
 }
