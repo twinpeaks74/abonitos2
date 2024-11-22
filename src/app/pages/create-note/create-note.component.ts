125068dad4bcbf281d17d0f00e889d4fe3f332ba
oliver zulett <oliverzulett@gmail.com>
2024-11-22 14:46:26 -0400

    estilo de error en formulario invalido

--- src/app/pages/create-note/create-note.component.ts
+++ src/app/pages/create-note/create-note.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { ToolBarComponent } from '../../components/tool-bar/tool-bar.component';
 import { Note } from '../../components/card-list/card-list.component';
 import { FormsModule, NgForm } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-create-note',
-  imports: [ToolBarComponent, FormsModule],
+  imports: [ToolBarComponent, FormsModule, CommonModule],
   templateUrl: './create-note.component.html',
   styleUrl: './create-note.component.css'
 })
