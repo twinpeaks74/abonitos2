580f28a9b69e5dedabb04ba14b937b361b8f0d82
oliver zulett <oliverzulett@gmail.com>
2024-11-21 22:17:05 -0400

    lista de tarjetas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
+import { CardListComponent } from './card-list/card-list.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, CardListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
   standalone: true
