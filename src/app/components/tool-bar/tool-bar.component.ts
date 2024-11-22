084f7042a45de9fd41fa03e7136d99a8072da2d6
oliver zulett <oliverzulett@gmail.com>
2024-11-22 13:47:51 -0400

    refactorizacion y uso del toolbar

--- src/app/components/tool-bar/tool-bar.component.ts
+++ src/app/components/tool-bar/tool-bar.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-tool-bar',
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: './tool-bar.component.html',
   styleUrl: './tool-bar.component.css'
 })
 export class ToolBarComponent {
-
+  @Input() showBackBtn = false;
+  @Input() showCreateBtn = false;
+  @Input() title = '';
 }
--- src/app/pages/create-note/create-note.component.ts
+++ src/app/pages/create-note/create-note.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { ToolBarComponent } from '../../components/tool-bar/tool-bar.component';
 
 @Component({
   selector: 'app-create-note',
-  imports: [],
+  imports: [ToolBarComponent],
   templateUrl: './create-note.component.html',
   styleUrl: './create-note.component.css'
 })
