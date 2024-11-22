59c661df0364a2ac552b306210f6344c19dda6f1
oliver zulett <oliverzulett@gmail.com>
2024-11-22 11:17:58 -0400

    creacion de toolbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { CardListComponent } from './card-list/card-list.component';
+import { ToolBarComponent } from './tool-bar/tool-bar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, CardListComponent],
+  imports: [RouterOutlet, HeaderComponent, CardListComponent, ToolBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
   standalone: true
---
+++ src/app/tool-bar/tool-bar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tool-bar',
+  imports: [],
+  templateUrl: './tool-bar.component.html',
+  styleUrl: './tool-bar.component.css'
+})
+export class ToolBarComponent {
+
+}
