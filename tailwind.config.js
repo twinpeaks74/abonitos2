275f8444aa92c021dd607c7b9c9279c493e2b2d3
twinpeaks74 <twinpeaks74@gmail.com>
2024-12-14 12:04:50 -0400

    modificacion de tema
    se modifico

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = {
     extend: {},
   },
   daisyui: {
-    themes: ["light", "dark", "cupcake"],
+    themes: ["light", "dark", "cupcake","synthwave"],
   },
   plugins: [
     require('daisyui'),
